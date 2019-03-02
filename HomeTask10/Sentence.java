37ecf84b37a158dac9eb8d0128afee23a8489646
Anastasiya Kibisova <anastasiyakibisova@gmail.com>
2019-03-02 23:25:26 +0300

    HomeTask10

---
+++ HomeTask10/Sentence.java
@@ -0,0 +1,21 @@
+﻿package core;
+
+public class Sentence extends Word {
+
+	void wordPlitting(String str, int n) {
+		char[] wordChars = str.toCharArray();
+		strt = "";
+		for (int i = 0; i < str.length(); i++) {
+			if (String.valueOf(wordChars[i]).compareTo(String.valueOf(' ')) != 0
+					&& String.valueOf(wordChars[i]).compareTo(String.valueOf('.')) != 0
+					&& String.valueOf(wordChars[i]).compareTo(String.valueOf('!')) != 0
+					&& String.valueOf(wordChars[i]).compareTo(String.valueOf('?')) != 0) {
+				strt += wordChars[i];
+			} else {
+				characterReplacement(strt + wordChars[i], n);
+				strt = "";
+			}
+		}
+
+	}
+}
