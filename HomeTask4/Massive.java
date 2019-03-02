2961e19729ffa6a76f2bf9de33097ac643400072
Anastasiya Kibisova <anastasiyakibisova@gmail.com>
2019-03-02 22:53:08 +0300

    HomeTask4

---
+++ HomeTask4/Massive.java
@@ -0,0 +1,34 @@
+﻿package core;
+
+public class Massive {
+
+	public void printMassiveAsLine(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public void printReverseMassiveAsLine(int[] array) {
+		for (int i = array.length - 1; i >= 0; i--) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public int getSumOfElements(int[] array) {
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum = sum + array[i];
+		}
+		return sum;
+	}
+
+	public int[] multiptyBy3(int[] array) {
+		int multiply = 3;
+		for (int i = 0; i < array.length; i++) {
+			array[i] = array[i] * multiply;
+		}
+		return array;
+	}
+}
---
+++ HomeTask4/Task4_1.java
@@ -0,0 +1,21 @@
+﻿package core;
+
+public class Task4_1 {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.setName("Anastasiya");
+		person.setSurname("Kibisova");
+		person.setAge(15);
+		person.setPhone(5555555);
+		System.out.println(person.getName());
+		System.out.println(person.getSurname());
+		System.out.println(person.getAge());
+		System.out.println(person.getPhone());
+		person.printAllInformation();
+		person.printName();
+		System.out.println(person.isAdult());
+
+	}
+
+}
