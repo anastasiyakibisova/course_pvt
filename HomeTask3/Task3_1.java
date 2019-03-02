d4745a32d455d29c5d249bf05d17899311e2fe47
Anastasiya Kibisova <anastasiyakibisova@gmail.com>
2019-03-02 22:18:00 +0300

    HomeTask3

---
+++ HomeTask3/Task3_1.java
@@ -0,0 +1,25 @@
+﻿package core;
+
+import java.util.Scanner;
+
+public class Task3_1 {
+
+	public static void main(String[] args) {
+		// Найти минимальное число из 2 чисел, вернуть минимальное
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input number");
+		int number1 = scanner.nextInt();
+		int number2 = scanner.nextInt();
+		System.out.println(minNumber(number1, number2));
+	}
+
+	public static int minNumber(int a, int b) {
+		if (a < b) {
+			return a;
+		} else {
+			return b;
+		}
+
+	}
+
+}
---
+++ HomeTask3/Task3_4.java
@@ -0,0 +1,19 @@
+﻿package core;
+
+import java.util.Scanner;
+
+public class Task3_4 {
+
+	public static void main(String[] args) {
+		// Возвести число в куб, вернуть результат возведения
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input number");
+		int number = scanner.nextInt();
+		System.out.println("Cube Number = " + cubNumber(number));
+	}
+
+	public static int cubNumber(int a) {
+		return a * a * a;
+	}
+
+}
