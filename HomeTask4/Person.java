be1a3f5faa0c6adaa01b49fc8534a3fc71a928e3
Anastasiya Kibisova <anastasiyakibisova@gmail.com>
2019-03-02 22:53:08 +0300

    HomeTask4

---
+++ HomeTask4/Person.java
@@ -0,0 +1,56 @@
+﻿package core;
+
+public class Person {
+
+	public String name;
+	public String surname;
+	public int age;
+	public int phone;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public int getPhone() {
+		return this.phone;
+	}
+
+	public void printAllInformation() {
+		String message = "Name is %s, Surname is %s, Age is %s, Phone is %s";
+		System.out.println(getName() + getSurname() + getAge() + getPhone());
+	}
+
+	public void printName() {
+		String message = "Name is %s, Surname is %s";
+		System.out.println(getName() + getSurname());
+	}
+
+	public boolean isAdult() {
+		return getAge() > 18? true: false;
+		}
+	}
+}
---
+++ HomeTask4/Task4_2.java
@@ -0,0 +1,30 @@
+﻿package core;
+
+public class Task4_2 {
+
+	public static void main(String[] args) {
+		// Создать массив array с данными
+		int size = 10;
+		int underInterval = 1;
+		int topInterval = 5;
+		int[] array = new int[size];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = underInterval + (int) (Math.random() * (topInterval - underInterval));
+		}
+		// Создать объект класса Massive
+		Massive massive = new Massive();
+		// Используя методы класса Massive вывести созданный выше array в прямом и
+		// обратном порядке
+		massive.printMassiveAsLine(array);
+		massive.printReverseMassiveAsLine(array);
+		// Используя методы класса Massive вывести сумму элементов array
+		System.out.println(massive.getSumOfElements(array));
+		// Используя методы класса Massive умножить созданный array на 3
+		massive.multiptyBy3(array);
+		// Используя методы класса Massive вывести полученный после умножения array в
+		// прямом и обратном порядке
+		massive.printMassiveAsLine(array);
+		massive.printReverseMassiveAsLine(array);
+	}
+
+}
