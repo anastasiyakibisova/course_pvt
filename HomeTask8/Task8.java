ae01234c7960b948fe7c69ff6e1660fb722d05fa
Anastasiya Kibisova <anastasiyakibisova@gmail.com>
2019-03-02 23:14:40 +0300

    HomeTask8

---
+++ HomeTask8/Task8.java
@@ -0,0 +1,18 @@
+﻿package core;
+
+public class Task8 {
+
+	public static void main(String[] args) {
+		Tester tester = new Tester();
+		Tester tester = new Tester("Petr", "Petrov", 10, "B1", 1000);
+		tester.setSalary(15000);
+		System.out.println(tester.getSalary());
+		tester.multiplyByTwo(100); // private -доступн только в созданном классе
+		System.out.println(tester.experienceInMonths(2));
+		tester.printNameSurname();
+		System.out.println();
+		tester.allInformation();
+
+	}
+
+}
---
+++ HomeTask8/Task8_1.java
@@ -0,0 +1,20 @@
+﻿package core1;
+
+import core.Tester;
+
+public class Task8_1 {
+
+	public static void main(String[] args) {
+		Tester tester = new Tester("Petr", "Petrov", 10, "B1", 1000);
+		tester.setSalary(15000); // friendly доступен классам, находящимся в том же пакете.
+		System.out.println(tester.getSalary());// friendly доступен классам, находящимся в том же пакете.
+		tester.multiplyByTwo(100); // private -доступн только в созданном классе
+		System.out.println(tester.experienceInMonths(2));// friendly доступен классам, находящимся в том же пакете.
+		tester.printNameSurname();// protected доступен классам, находящимся в том же пакете, и подклассам – в
+									// других пакетах
+		System.out.println();
+		tester.allInformation();
+
+	}
+
+}
