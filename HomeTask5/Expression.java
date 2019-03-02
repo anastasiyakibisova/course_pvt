a286aa3c70133bf72bc5d9beddc3663e9ad44141
Anastasiya Kibisova <anastasiyakibisova@gmail.com>
2019-03-02 23:01:51 +0300

    HomeTask5

---
+++ HomeTask5/Expression.java
@@ -0,0 +1,20 @@
+﻿package core;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+public class Expression {
+
+	public static boolean test(String testString) {
+		Pattern p = Pattern.compile("[a-zA-Z]{0,4}");
+		Matcher m = p.matcher(testString);
+		return m.matches();
+	}
+	
+	public static boolean test1(String testString) {
+		Pattern p = Pattern.compile(".{4}");
+		Matcher m = p.matcher(testString);
+		return m.matches();
+	}
+}
