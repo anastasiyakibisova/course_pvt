e511f74f541f56cbdb0baf8ded2a7abd295b29a9
Anastasiya Kibisova <anastasiyakibisova@gmail.com>
2019-03-03 10:43:30 +0300

    HomeTask11

---
+++ HomeTask11/Dropdown.java
@@ -0,0 +1,21 @@
+﻿package core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dropdown {
+
+	private boolean isSelected;
+	private String name;
+	private List<String> options = new ArrayList<String>();
+
+	public Dropdown(boolean isSelected, String name, List<String> options) {
+		this.isSelected = isSelected;
+		this.name = name;
+		this.options = options;
+	}
+
+	public String toString() {
+		return this.isSelected + " " + this.name + " " + this.options;
+	}
+}
---
+++ HomeTask11/Field.java
@@ -0,0 +1,16 @@
+﻿package core;
+
+public class Field {
+
+	private int id;
+	private int value;
+
+	public Field(int id, int value) {
+		this.id = id;
+		this.value = value;
+	}
+
+	public String toString() {
+		return this.id + " " + this.value;
+	}
+}
