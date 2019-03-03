a9b7b7bbe78a413d7189b04452956e3b495d2610
Anastasiya Kibisova <anastasiyakibisova@gmail.com>
2019-03-03 10:47:32 +0300

    HomeTask13

---
+++ HomeTask13/HomeTask13.java
@@ -0,0 +1,27 @@
+﻿package core;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HomeTask13 {
+
+	public static void main(String[] args) {
+
+		Massiv massive = new Massiv();
+		int[] array = { 1, 0, 10, -5 };
+		massive.sumMas(array, 5);
+		massive.getMas(array, 5);
+		massive.divisionByNumber(array);
+		massive.minElements(array);
+		massive.mas(array);
+		try (FileReader fr = new FileReader("file.txt")) {
+			char[] mas = new char[9];
+			fr.read(mas);
+			for (char c : mas)
+				System.out.print(c);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ HomeTask13/Massiv.java
@@ -0,0 +1,76 @@
+﻿package core;
+
+public class Massiv {
+
+	private int min;
+	private int div = 0;
+	private int sum = 0;
+
+	public void sumMas(int[] mas, int size) {
+		try {
+			for (int i = 0; i < size; i++) {
+				sum += mas[i];
+			}
+		} catch (ArrayIndexOutOfBoundsException x) {
+			System.out.println("out of bounds array");
+		} finally {
+			System.out.println("the amount is not counted");
+		}
+	}
+
+	public void getMas(int[] mas, int size) {
+		try {
+			for (int i = 0; i < size; i++) {
+				System.out.print(mas[i] + " ");
+			}
+			System.out.println();
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			System.out.println("out of bounds array");
+		}
+	}
+
+	public void divisionByNumber(int[] mas) {
+		try {
+			for (int i = 0; i <= mas.length; i++) {
+				System.out.println(mas[i] / div);
+			}
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			System.out.println("out of bounds array");
+		} catch (ArithmeticException ex) {
+			System.out.println("Division by zero");
+		}
+	}
+
+	public void minElements(int[] mas) {
+		min = mas[0];
+		try {
+			for (int i = 0; i <= mas.length; i++) {
+				if (mas[i] < min) {
+					min = mas[i];
+				}
+			}
+			System.out.println(min / div);
+		} catch (ArrayIndexOutOfBoundsException | ArithmeticException ex) {
+			System.out.println("error " + ex.getMessage());
+		}
+	}
+
+	public void mas(int[] mas) {
+		try {
+			try {
+				for (int i = 0; i <= mas.length; i++) {
+					System.out.print(mas[i] + " ");
+				}
+			} catch (ArithmeticException ex) {
+				System.out.println("out of bounds array");
+			}
+			for (int i = 0; i < mas.length; i++) {
+				System.out.println(mas[i] / (mas[i] / mas[i]));
+			}
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			System.out.println("Division by zero");
+		}
+
+	}
+
+}
